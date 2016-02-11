8a69d47f9d37d8e7067f8f15d25797d7d43b087b
RYAN OGLE <ryanogle@RYANs-MacBook-Air-2.local>
2016-02-11 01:24:10 -0800

    stuff

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var ioEmitter= require('socket.io-emitter')
 
 
 module.exports = function(options){
+  var dataSource = options.dataSource;
   console.log('SETTING UP THE IO ADAPTER');
   setTimeout(function(){
     console.log('TIMEOUT DONE');
-    var io = ioEmitter( {host: options.host, port: options.port} );
+    var io = ioEmitter( {host: dataSource.host, port: dataSource.port} );
     io.emit('someroom', 'bob');
     io.emit('alive', 'bob');
     io.emit('alive', 'bob');
